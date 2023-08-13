fba57cad85684bc353f76b693c5d6265fcde67e4
chadkraus87 <127354484+chadkraus87@users.noreply.github.com>
2023-08-13 11:46:05 -0500

    Working on About.js. Added headshot to image assets.

--- portfolio-app/src/pages/About.js
+++ portfolio-app/src/pages/About.js
@@ -1,3 +1,24 @@
 import React from "react";
 
 
+const About = () => {
+    return (
+      <section id="about" className="py-5">
+        <div className="container">
+          <h2 className="text-center mb-4">About Me</h2>
+          <div className="row">
+            <div className="col-md-6">
+              <img src="Chadwick-Kraus-headshot.jpg" alt="Profile" className="img-fluid rounded-circle mb-4" />
+            </div>
+            <div className="col-md-6">
+              <p>
+                {/* My bio */}
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  };
+  
+  export default About;
