d7aa574e2bd7d491175ae1c194bc8900b561366d
chadkraus87 <127354484+chadkraus87@users.noreply.github.com>
2023-08-12 20:31:21 -0500

    Added note to remember to add pdf file of resume to files folder in Resume.js

--- portfolio-app/src/pages/Resume.js
+++ portfolio-app/src/pages/Resume.js
@@ -1,6 +1,6 @@
 import React from "react";
 import myResume from "../../assets/files/Chadwick-Kraus-Full-Stack-Developer.pdf";
-
+// Add resume to assets files folder
 
 function Resume() {
     return (
