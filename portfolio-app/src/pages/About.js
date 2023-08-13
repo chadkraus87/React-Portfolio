12bd9aa898a6da28324e6c8b8d2e3dfc71b80cc8
chadkraus87 <127354484+chadkraus87@users.noreply.github.com>
2023-08-13 12:00:57 -0500

    Working on building pages/debugging.

--- portfolio-app/src/pages/About.js
+++ portfolio-app/src/pages/About.js
@@ -1,24 +1,24 @@
 import React from "react";
-
+import profileImage from "../assets/images/Chadwick-Kraus-headshot.jpg";
 
 const About = () => {
-    return (
-      <section id="about" className="py-5">
-        <div className="container">
-          <h2 className="text-center mb-4">About Me</h2>
-          <div className="row">
-            <div className="col-md-6">
-              <img src="Chadwick-Kraus-headshot.jpg" alt="Profile" className="img-fluid rounded-circle mb-4" />
-            </div>
-            <div className="col-md-6">
-              <p>
-                {/* My bio */}
-              </p>
-            </div>
+  return (
+    <section id="about" className="py-5">
+      <div className="container">
+        <h2 className="text-center mb-4">About Me</h2>
+        <div className="row">
+          <div className="col-md-6">
+            <img src={profileImage} alt="Profile" className="img-fluid rounded-circle mb-4" />
+          </div>
+          <div className="col-md-6">
+            <p>
+              {/* Add bio */}
+            </p>
           </div>
         </div>
-      </section>
-    );
-  };
-  
-  export default About;
+      </div>
+    </section>
+  );
+};
+
+export default About;
