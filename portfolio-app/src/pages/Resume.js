6b5576b7266013af95731d49b45decae0ccd732d
chadkraus87 <127354484+chadkraus87@users.noreply.github.com>
2023-08-14 11:05:47 -0500

    Added resume pdf to files folder and completed Resume.js

--- portfolio-app/src/pages/Resume.js
+++ portfolio-app/src/pages/Resume.js
@@ -1,8 +1,18 @@
 import React from "react";
 import myResume from "../../assets/files/Chadwick-Kraus-Full-Stack-Developer.pdf";
-// Add resume to assets files folder
 
 function Resume() {
-    return (
-        <div></div>
-           ) }
+  return (
+    <div className="container py-5">
+      <h2 className="text-center mb-4">Resume</h2>
+      <iframe
+        src={myResume}
+        title="Chadwick Kraus Resume"
+        width="100%"
+        height="800px"
+      ></iframe>
+    </div>
+  );
+}
+
+export default Resume;
