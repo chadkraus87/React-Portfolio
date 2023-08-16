d24a14439a787c17f3cc781f9bf9e5be33bf1f14
chadkraus87 <127354484+chadkraus87@users.noreply.github.com>
2023-08-16 08:41:35 -0500

    Added fontawesome icons to footer.

--- portfolio-app/src/components/Footer.js
+++ portfolio-app/src/components/Footer.js
@@ -11,16 +11,16 @@ const Footer = () => {
               <div className="collapse navbar-collapse">
                 <ul className="navbar-nav ml-auto">
                   <li className="nav-item">
-                    <Link to="/" className="nav-link text-black">About</Link>
+                    <Link to="/" className="nav-link text-black">About <i class="fa-solid fa-person nav-icon"></i></Link>
                   </li>
                   <li className="nav-item">
-                    <Link to="/portfolio" className="nav-link text-black">Portfolio</Link>
+                    <Link to="/portfolio" className="nav-link text-black">Portfolio <i class="fa-solid fa-folder nav-icon"></i></Link>
                   </li>
                   <li className="nav-item">
-                    <Link to="/resume" className="nav-link text-black">Resume</Link>
+                    <Link to="/resume" className="nav-link text-black">Resume <i class="fa-solid fa-file nav-icon"></i></Link>
                   </li>
                   <li className="nav-item">
-                    <Link to="/contact" className="nav-link text-black">Contact</Link>
+                    <Link to="/contact" className="nav-link text-black">Contact <i class="fa-solid fa-file-signature nav-icon"></i></Link>
                   </li>
                 </ul>
               </div>
